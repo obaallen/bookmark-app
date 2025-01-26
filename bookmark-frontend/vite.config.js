e589bc12ceb0bbffa950f313687e829a75946286
Allen Obayagbona <35696261+obaallen@users.noreply.github.com>
2025-01-25 22:43:19 -0800

    Updating Title etc

--- bookmark-frontend/vite.config.js
+++ bookmark-frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "127.0.0.1", // Specify the host explicitly
-    port: 5173, // Optional: explicitly set the port
+    host: "127.0.0.1", 
+    port: 5173, 
     proxy: {
       "/api": {
         target: "http://127.0.0.1:5000", // Flask backend
